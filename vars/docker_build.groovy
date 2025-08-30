13d7f867c527ff313e3fc8c28f82f46299483ea9
SANDEEP MOHAPATRA <sandeepmohapatra177@gmail.com>
2025-08-30 21:37:01 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call(String ImageTag, String ImageName, String dockerHubUser){
-  sh "docker build -t ${dockerHubUser}/${ImageName}:${ImageTag} ."
+  sh "docker build -t ${dockerHubUser}/${ImageName}:${ImageTag} ./django-notes-app"
   
 }
