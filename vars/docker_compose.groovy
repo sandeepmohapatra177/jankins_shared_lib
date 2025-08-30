01034e4e1ed0f5c0211e608cffce8edc778cebed
SANDEEP MOHAPATRA <sandeepmohapatra177@gmail.com>
2025-08-30 23:09:25 +0530

    Update docker_compose.groovy

--- vars/docker_compose.groovy
+++ vars/docker_compose.groovy
@@ -1,3 +1,3 @@
 def call(){
-  sh "docker compose down && docker compose -f django-notes-app/docker-compose.yml up -d"
+  sh "docker-compose down && docker compose -f django-notes-app/docker-compose.yml up -d"
 }
